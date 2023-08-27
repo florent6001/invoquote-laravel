231ef8a125339d9a31d33b591b1caffd54cf861f
Florent Vandroy <florentvandroy@gmail.com>
2023-08-27 13:42:08 +0200

    update meta description

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description"
-        content="Gérez vos activités freelance sans effort, générez des devis et des factures professionnels. Prenez le contrôle de votre entreprise et concentrez-vous sur ce que vous faites de mieux.">
+        content="Gérez votre activité freelance avec efficacité et professionnalisme. Générez des devis et des factures en quelques clics, et concentrez-vous sur ce que vous faites de mieux.">
 
     <title>@yield('title') - InvoQuote</title>
 
